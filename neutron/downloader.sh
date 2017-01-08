4be35d9b0ff957482048f7571cffa20ea12591f7
Sam Yaple <sam@yaple.net>
2017-01-08 20:09:35 +0000

    Add neutron build

---
+++ neutron/downloader.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+export REPO="samyaple/openstack-requirements"
+export TOKEN=$(curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${REPO}:pull" | python -c "import sys, json; print json.load(sys.stdin)['token']")
+export BLOB=$(curl -H "Authorization: Bearer ${TOKEN}" https://registry.hub.docker.com/v2/${REPO}/manifests/latest | python -c "import sys, json; print json.load(sys.stdin)['fsLayers'][0]['blobSum']")
+
+curl -L -H "Authorization: Bearer ${TOKEN}" https://registry.hub.docker.com/v2/${REPO}/blobs/${BLOB} > /tmp/wheels.tar.gz
+
+tar xvf /tmp/wheels.tar.gz -C /tmp/ packages
