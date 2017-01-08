f1cfed4cefbca4b587d50a185530282cd41e2ac9
Sam Yaple <sam@yaple.net>
2017-01-08 23:09:24 +0000

    Pick correct layer

--- keystone/downloader.sh
+++ keystone/downloader.sh
@@ -2,7 +2,7 @@
 
 export REPO="samyaple/openstack-requirements"
 export TOKEN=$(curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${REPO}:pull" | python -c "import sys, json; print json.load(sys.stdin)['token']")
-export BLOB=$(curl -H "Authorization: Bearer ${TOKEN}" https://registry.hub.docker.com/v2/${REPO}/manifests/latest | python -c "import sys, json; print json.load(sys.stdin)['fsLayers'][1]['blobSum']")
+export BLOB=$(curl -H "Authorization: Bearer ${TOKEN}" https://registry.hub.docker.com/v2/${REPO}/manifests/latest | python -c "import sys, json; print json.load(sys.stdin)['fsLayers'][0]['blobSum']")
 
 curl -L -H "Authorization: Bearer ${TOKEN}" https://registry.hub.docker.com/v2/${REPO}/blobs/${BLOB} > /tmp/wheels.tar.gz
 
